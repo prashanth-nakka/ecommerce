a19538d31012d0fd0cc405765ea1546a328e4cc2
Tooni <prashanth04@gmail.com>
2022-08-08 00:07:07 +0530

    Module - 2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,6 +3,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR/'static/images')
+MEDIA_URL = '/images/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- store/views.py
+++ store/views.py
@@ -1,19 +1,50 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 # USING FUNCTION BASED VIEWS
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
     return render(request, 'store/store.html', context)
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        print(customer)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        print(order)
+        items = order.orderitem_set.all()
+        print(items)
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, }
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        print(customer)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        print(order)
+        items = order.orderitem_set.all()
+        print(items)
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, }
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'store/checkout.html', context)
